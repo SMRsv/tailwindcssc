2bf9f49bbbd09e37404c123c277719726a6e2e7b
SMRsv <semramirez127@gmail.com>
2022-02-16 18:05:05 -0600

    @Layer, Variants, Functions: Screen-Theme, PostCSS, JIT, [], Theme(1,52,19)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
+  mode: 'jit',
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '600px',
+      md: '700px',
+      lg: '900px',
+      xl: '1200px'
+    },
+    extend: {
+      colors: {
+        indigocust: {
+          light: '#b3bcf5',
+          DEFAULT: '#5c6ac4',
+          dark: '#202e78',
+        },
+      },
+      spacing:{
+        1: '0.55rem'
+      },
+    },
   },
   plugins: [],
 }
